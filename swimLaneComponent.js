dfc2ae9ca661549399f8031b0fe465a09e8055e0
savinu <savinu.vijay@gmail.com>
2022-02-23 08:07:32 +0530

    styled Add buttons

--- swimLaneComponent.js
+++ swimLaneComponent.js
@@ -8,7 +8,7 @@ swimLaneTemplate.innerHTML = `
         <span class="title-display" >New</span>
       </div>
       <div class=tasks></div>
-      <button class="add-task-btn">Add Task</button>
+      <button class="add-task-btn">+</button>
     </div>
 `;
 
