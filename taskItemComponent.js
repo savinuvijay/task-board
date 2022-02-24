4187cebc7ff7507a8ff8f1e1e70b3b0d2e612a79
savinu <savinu.vijay@gmail.com>
2022-02-24 08:33:32 +0530

    added task title

--- taskItemComponent.js
+++ taskItemComponent.js
@@ -2,6 +2,11 @@ const taskItemTemplate = document.createElement("template");
 taskItemTemplate.innerHTML = `
     <link rel="stylesheet" href="taskItemStyle.css" />
     <div class="task-item" draggable="true">
+        <div class="task-title">
+            <input class="title-input" type="text" value="New"/>
+            <button class="title-ok">ok</button>
+            <span class="title-display" >New</span>
+        </div>
         <textarea class="task-details"></textarea>
         <button class="delete-btn">Delete</button>
     </div>
@@ -12,12 +17,26 @@ export class TaskItem extends HTMLElement {
         super();
         this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(taskItemTemplate.content.cloneNode(true));
-        this.deleteBtn = this.shadowRoot.querySelector(".delete-btn");
+
+        this.taskItem = this.shadowRoot.querySelector(".task-item");
+
+        this.taskTitle = this.taskItem.querySelector(".task-title");
+        this.titleInput = this.taskTitle.querySelector(".title-input");
+        this.titleOk = this.taskTitle.querySelector(".title-ok");
+        this.titleDisplay = this.taskTitle.querySelector(".title-display");
+
+        this.titleInput.style.display = "none";
+        this.titleOk.style.display = "none";
+        this.titleDisplay.style.display = "inline";
+
+        this.deleteBtn = this.taskItem.querySelector(".delete-btn");
 
         this.taskDeleteEvent = new CustomEvent("taskdelete", {});
     }
 
     connectedCallback() {
+        this.titleDisplay.addEventListener("click", (e) => this.editTitle(e));
+        this.titleOk.addEventListener("click", (e) => this.saveTitle(e));
         this.deleteBtn.addEventListener("click", (e) => this.deleteTask(e));
     }
 
@@ -28,6 +47,24 @@ export class TaskItem extends HTMLElement {
         // this.details.dropZone = null;
         // this.details.task = e;
         this.dispatchEvent(this.taskDeleteEvent);
+    }
+
+    editTitle(e) {
+        e.stopPropagation();
+
+        this.titleInput.style.display = "inline";
+        this.titleOk.style.display = "inline";
+        this.titleDisplay.style.display = "none";
+    }
+
+    saveTitle(e) {
+        e.stopPropagation();
+
+        this.titleDisplay.innerHTML = this.titleInput.value;
+
+        this.titleInput.style.display = "none";
+        this.titleOk.style.display = "none";
+        this.titleDisplay.style.display = "inline";
     }
 
     disconnectedCallback() {
